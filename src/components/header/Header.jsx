4ae691213470061a30fddda795306ebb6d79e1e2
Good_Cthulhu <hayyabusa_01@mail.ru>
2022-11-12 06:27:40 +0300

    adaptive block Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import logo from '../../images/logo.png';
 import logoText from '../../images/logo-text.png';
-import play from '../../images/play.svg'
+import play from '../../images/play.svg';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
+
 
 import Nav from '../nav/Nav';
 import Logo from './Logo';
@@ -11,20 +14,21 @@ const Header = () => {
     <header className="header">
       <div className="container">
         <div className="header__inner">
-          <Logo/>
+          <Logo />
+          <GiHamburgerMenu className='burger'/>
           <Nav />
         </div>
         <section className="header__content">
-            <h1 className='header__title'>New Single</h1> 
-            <h2 className='header__description'>War For Love</h2>
-            <div className="player">
-                <img src={play} alt="play" className='player-play'/>
-                <span className="line"></span>
-                <div className="player-timer">
-                    <span className="time-start">00:22-</span>
-                    <span className="time-end">02:54</span>
-                </div>
+          <h1 className="header__title">New Single</h1>
+          <h2 className="header__description">War For Love</h2>
+          <div className="player">
+            <img src={play} alt="play" className="player-play" />
+            <span className="line"></span>
+            <div className="player-timer">
+              <span className="time-start">00:22-</span>
+              <span className="time-end">02:54</span>
             </div>
+          </div>
         </section>
       </div>
     </header>
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,34 +1,54 @@
 import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Nav = () => {
+  function showSettings(event) {
+    event.preventDefault();
+  }
   return (
-    <nav className="nav">
-      <ul className="nav__list">
-        <li className="nav__item">
-          <a className="nav__link" href="#">About</a>
-        </li>
+    <div className="nav__menu">
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a className="nav__link" href="# ">
+              About
+            </a>
+          </li>
 
-        <li className="nav__item">
-          <a className="nav__link" href="#">News</a>
-        </li>
+          <li className="nav__item">
+            <a className="nav__link" href="# ">
+              News
+            </a>
+          </li>
 
-        <li className="nav__item">
-          <a className="nav__link" href="#">Music</a>
-        </li>
+          <li className="nav__item">
+            <a className="nav__link" href="# ">
+              Music
+            </a>
+          </li>
 
-        <li className="nav__item">
-          <a className="nav__link" href="#">Media</a>
-        </li>
+          <li className="nav__item">
+            <a className="nav__link" href="# ">
+              Media
+            </a>
+          </li>
 
-        <li className="nav__item">
-          <a className="nav__link" href="#">Tours</a>
-        </li>
+          <li className="nav__item">
+            <a className="nav__link" href="# ">
+              Tours
+            </a>
+          </li>
 
-        <li className="nav__item">
-          <a className="nav__link" href="#">Contacts</a>
-        </li>
-      </ul>
-    </nav>
+          <li className="nav__item">
+            <a className="nav__link" href="# ">
+              Contacts
+            </a>
+          </li>
+        </ul>
+      </nav>
+    
+    </div>
   );
 };
 
